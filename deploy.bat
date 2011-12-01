1468a7866300dcb135b733b2096712f32acec1ac
mrichter <mark.richter@thoughtworks.com>
2011-12-01 15:18:03 -0800

    add batch file (deploy.bat) to copy artifacts for my local project

---
+++ deploy.bat
@@ -0,0 +1,5 @@
+@echo off
+copy /Y .\build\thoughtworksminglelib.dll ..\vsc\lib\"
+copy /Y .\build\thoughtworksminglelib.pdb ..\vsc\lib\"
+copy /Y .\build\thoughtworkscorelib.dll ..\vsc\lib\"
+copy /Y .\build\thoughtworkscorelib.pdb ..\vsc\lib\"
