adf6bc43a560f71bc1334afe1b8ea9e356189802
mrichter <mark.richter@thoughtworks.com>
2012-02-07 09:49:41 -0800

    separate some tests into separate integration_spec file

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,17 +1,33 @@
 require File.dirname(__FILE__)+'/spec_helper'
 require 'fake_mingle'
 
-describe 'card' do
-    before do
-        host = "http://localhost:8080"
-        user = "mingleuser"
-        pw = "secret"
-        @project = MingleServer.new(host, user, pw).get_project("apitest")
+describe 'project' do
+
+    it 'creates a card' do
+        proj = MingleServer.new("http://localhost:8080","mingleuser","secret").GetProject("apitest")
+        card = proj.create_card("story", "This is a new card")
+        card.type.should == "Story" 
+    end
+
+    it 'should get a property of managed list of text items' do
+        proj = MingleServer.new("http://localhost:8080", "mingleuser", "secret").GetProject("apitest")
+        card = proj.get_card(120)
+        card.card_properties["Priority"].value.should == "Essential"
+        card.card_properties["Story Status"].value.should == "New"
+        card.card_properties["Owner"].value.should == "mingleuser"
     end
     
-    it 'update invalid property name should fail' do
-        card = @project.get_card(7)
-        card.add_property_filter_to_post_data("bogus", "value")
-        lambda {card.update()}.should raise_error(MingleWebException)
+    it 'should get results from execute_mql' do
+        proj = MingleServer.new("http://localhost:8080", "mingleuser", "secret").GetProject("apitest")
+        results = proj.exec_mql("select count(number)")
+        proj.results_count(results).should == 1
+    end    
+    
+    it 'posts a murmur' do
+        now = System::DateTime.now
+        proj = MingleServer.new("http://localhost:8080", "mingleuser", "secret").GetProject("apitest")
+        count = proj.get_murmurs.count
+        proj.send_murmur("I was murmured at " + now.to_string)
+        proj.get_murmurs.count.should == count + 1
     end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -63,32 +63,4 @@ describe 'mingle_project' do
         @project.get_card(117).type.should == "Task"
     end
     
-    it 'creates a card' do
-        proj = MingleServer.new("http://localhost:8080","mingleuser","secret").GetProject("apitest")
-        card = proj.create_card("story", "This is a new card")
-        card.type.should == "Story" 
-    end
-
-    it 'should get a property of managed list of text items' do
-        proj = MingleServer.new("http://localhost:8080", "mingleuser", "secret").GetProject("apitest")
-        card = proj.get_card(120)
-        card.card_properties["Priority"].value.should == "Essential"
-        card.card_properties["Story Status"].value.should == "New"
-        card.card_properties["Owner"].value.should == "mingleuser"
-    end
-    
-    it 'should get results from execute_mql' do
-        proj = MingleServer.new("http://localhost:8080", "mingleuser", "secret").GetProject("apitest")
-        results = proj.exec_mql("select count(number)")
-        proj.results_count(results).should == 1
-    end    
-    
-    it 'posts a murmur' do
-        now = System::DateTime.now
-        proj = MingleServer.new("http://localhost:8080", "mingleuser", "secret").GetProject("apitest")
-        count = proj.get_murmurs.count
-        #System::Diagnostics::Debugger.break
-        proj.send_murmur("I was murmured at " + now.to_string)
-        proj.get_murmurs.count.should == count + 1
-    end
 end
